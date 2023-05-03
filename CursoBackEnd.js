cf89c2f25117d9c7aae4a9e077d316a5acb45432
lorenzoo <enriquelorenzoo@gmail.com>
2023-05-03 16:20:53 +0200

    Update CursoBackEnd.js

--- CursoBackEnd.js
+++ CursoBackEnd.js
@@ -1,15 +1,28 @@
+const fs = require("fs");
+
+
 class ProductManager {
-  constructor() {
+  #path
+  constructor(path) {
     this.products = [];
+    this.#path = path;
+    if (!fs.existsSync(this.#path))
+    return fs.writeFileSync(this.#path, "[]")
+}
 
-  }
-
+ 
   getProducts() {
+    this.products = JSON.parse(fs.readFileSync(this.#path, "utf-8"))
     console.log(this.products);
     return this.products;
-  }
+}
+
+
+
   getProductById(id) {
-    const productFound = this.products.find((pro) => pro.id == id);
+
+    this.products = JSON.parse(fs.readFileSync(this.#path, "utf-8"))
+    const productFound = this.products.find(pro => pro.id == id);
     if (productFound) {
       return productFound;
     } else {
@@ -18,7 +31,9 @@ class ProductManager {
     }
   }
   #getProductByCode(code) {
-    const isInsArray = this.products.find((pro) => pro.code == code);
+
+    this.products = JSON.parse(fs.readFileSync(this.#path, "utf-8"))
+    const isInsArray = this.products.find(pro => pro.code === code);
     if (isInsArray) {
       return isInsArray;
     } else {
@@ -26,24 +41,11 @@ class ProductManager {
     }
   }
 
-  /*  #generateId() {
-      let Id = 0;
-      for (let i = 0; i < this.products.length; i++) {
-        const pro = this.products[i];
-         if (pro.id > newId)
-         {
-          Id = pro.id;
-        }
-        return ++Id;
-      }
-    }
-  */
 
   #generateId() {
 
-    //let newId = this.products.length;
-    //newId = this.products.length;
-    return this.products.length+1;
+
+    return this.products.length + 1;
   }
 
   addProduct(
@@ -54,7 +56,8 @@ class ProductManager {
     code,
     stock
   ) {
-    //const id = this.#generateId();
+
+    this.products = JSON.parse(fs.readFileSync(this.#path, "utf-8"))
 
     if (
       (title == undefined ||
@@ -84,7 +87,7 @@ class ProductManager {
     ) {
       console.log("Error, all fields are required, make sure this product is not adde in the system already");
     } else {
-      //const id = this.#generateId()
+      
       const newProduct = {
         title,
         description,
@@ -93,18 +96,50 @@ class ProductManager {
         code,
         stock,
         id: this.#generateId(),
+        
 
       };
-      // hasta aqui todo bien
+
       this.products = [...this.products, newProduct];
+      fs.writeFileSync(this.#path, JSON.stringify(this.products))
+
+      console.log("Product added");
       return true
     }
 
   }
+  //tarea_2 test delete and update product
+  deleteProduct(id) {
+    this.products = JSON.parse(fs.readFileSync(this.#path, "utf-8"))
+    const productFound = this.products.find(pro => pro.id == id);
+    if (this.products.find(p => p.id === id)) {
+      this.products.splice(this.products.indexOf(this.products.find(pro => pro.id === id)), 1)
+      fs.writeFileSync(this.#path, JSON.stringify(this.products))
+      return console.log("Deleted product successfully")
+      
+    } else {
+      return console.log("Product not found")
+    }
+  }
+
+  updateProduct(id, key, value) {
+    this.products = JSON.parse(fs.readFileSync(this.#path, "utf-8"))
+
+    if (key == "id") {
+      return console.log("It is not possible to modify the id field, try another field")
+    } else if (this.products.find(pro => pro.id === id)) {
+      const Found = this.products.find(pro => pro.id === id)
+      Found[key] = value
+      fs.writeFileSync(this.#path, JSON.stringify(this.products))
+      return console.log("Updated product successfully", Found)
+    } else {
+      return console.log("Product not found")
+    }
+  }
 }
 
 
-const testProduct = new ProductManager();
+const testProduct = new ProductManager("./product.json");
 
 testProduct.addProduct(
 
@@ -126,5 +161,8 @@ testProduct.addProduct(
   25_2
 );
 
+testProduct.getProducts();
 
-console.log(testProduct.products);
+console.log(testProduct.products); 
+testProduct.updateProduct(1, 'title', 'metal');
+testProduct.updateProduct(1, 'description', 'oro');
