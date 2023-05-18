fe8e35e96a337d1644bacc77353b3bc064826652
Enrique Segui <enriquesegui@Enriques-MacBook-Pro.local>
2023-05-18 06:27:26 +0200

    limpieza general de codigo

--- app.js
+++ app.js
@@ -25,31 +25,20 @@ app.get("/products", (req, res) => {
   console.log(req.query);
   const allProducts = productManager.getProducts();
   let limit = req.query.limit;
-  if (!limit){
+  if (!limit) {
     return res.json({
       products: allProducts
     });
-    
+
   }
 
-  /*TEST PARA ELIMINAR CODIGO Y LIMPIAR ( EN COMENTADO ) POR EL SIGUIENTE ELSE IF
 
-  const parsedLimit = parseInt(limit);
-  if (parsedLimit > 0 && parsedLimit <= allProducts.length)
-  const productsLimit = allProducts.slice(0, parsedLimit);{
-  return res.json({ data: productsLimit });
-}  return res.json({
-    status: "error",
-    msg: "Exceed the limit of products",
-  });
-});
-  */
 
-  else if (limit > 0 && limit <= allProducts.length){
-    let productsLimit = allProducts.slice (0 , limit);
-    return res.json ({data: productsLimit});
+  else if (limit > 0 && limit <= allProducts.length) {
+    let productsLimit = allProducts.slice(0, limit);
+    return res.json({ data: productsLimit });
   }
-  else if (limit > allProducts.length){
+  else if (limit > allProducts.length) {
     return res.json({
       status: "error",
       msg: "Exceed the limit of products",
@@ -57,28 +46,7 @@ app.get("/products", (req, res) => {
   }
 });
 
-// AQUI ES DONDE SE TIENE QUE PONER EL LIMITE Y FILTRARLO
-/*
-const price = req.query.price;
-  if (req.query && price) {
-    const productsFilterByPrice = productManager.getProducts().filter(p => p.price == req.query.price);
-    return res.json({
-      status: "success",
-      masg: "this are all of the products whit the price = " + price,
-      data: productsFilterByPrice,
 
-    })
-    //AQUI SE ENVIAN TODOS LOS PRODUCTOS EN CASO QUE NO ENCUENTRE QUE SE ESTA BUSCANDO----------------
-  } else {
-    return res.json({
-      status: "success",
-      msg: "this are all of the products we have",
-      data: productManager.getProducts(),
-    })
-  } 
-
-
-*/
 
 // LA BUSQUEDA POR ID " por PARAMS / " FUNCIONA --------------------------------------------------------------------------
 
@@ -94,7 +62,7 @@ app.get("/products/:id", (req, res) => {
     return res.json({
       status: "success",
       msg: "product found",
-      //tenemos que cambiar el data: porduct por data: productById (ProductManager)
+      
       data: product,
     });
   }
@@ -109,17 +77,12 @@ app.get("/products/:id", (req, res) => {
   }
 
 
-}); 
-
-
-
-
-// TEST DE RUTA este esta funcionando bien pero no la necesitamos ahora----------------------------------
-/*
-app.get("/boca", (req, res) => {
-  return res.json("el mejor del mundo");
 });
-*/
+
+
+
+
+
 
 // ESTA ES LA RUTA DE URLS QUE NO EXISTEN ---------------------------------------------------
 app.get("*", (req, res) => {
